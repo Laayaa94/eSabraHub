f8041075f5a25dcad2a54ea1505a7ff474e2406a
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2024-08-15 19:11:59 +0530

    Update backend

--- frontend/src/Pages/PostPage.jsx
+++ frontend/src/Pages/PostPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Post from '../Components/Login_Post_Profile/Post_Page/Post'
 
 const PostPage = () => {
   return (
     <div>
-      <Post />
+
     </div>
   )
 }
 
 export default PostPage
+
